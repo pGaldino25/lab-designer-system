4d8fe5dd594c90ff7304b7f2ebc0f575b436c22d
Paulo Galdino <pgaldino25@gmail.com>
2022-10-15 21:21:53 -0300

    Changed main.cjs in the field viteFinal

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -16,11 +16,11 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = '/lab-designer-system/'
-    }
+  // viteFinal: (config, { configType }) => {
+  //   if (configType === 'PRODUCTION') {
+  //     config.base = '/lab-designer-system/'
+  //   }
 
-    return config
-  }
+  //   return config
+  // }
 }
